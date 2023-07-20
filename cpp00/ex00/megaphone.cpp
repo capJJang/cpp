5afcdae33cf87887f5f3e33679495789e9c2b761
segan <>
2023-07-21 02:32:22 +0900

    finished

--- cpp00/ex00/megaphone.cpp
+++ cpp00/ex00/megaphone.cpp
@@ -1,5 +1,26 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   megaphone.cpp                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: segan <segan@student.42.fr>                +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/07/21 01:39:33 by segan             #+#    #+#             */
+/*   Updated: 2023/07/21 02:27:14 by segan            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include <iostream>
 
 int	main(int argc, char *argv[])
 {
+	if (argc == 1)
+		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE * ";
+	else
+	{
+		for (int i = 1; i < argc; i++)
+			std::cout << argv[i];
+	}
+	std::cout << std::endl;
+	return (0);
 }
