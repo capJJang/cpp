b455b5a15b02301cee1e2cc5609906efece8eb52
segan <segan@student.42seoul.kr>
2023-08-12 09:42:45 +0900

    fin : ex00

--- cpp01/ex00/Zombie.cpp
+++ cpp01/ex00/Zombie.cpp
@@ -1,22 +1,11 @@
 #include "Zombie.hpp"
+
 #include <iostream>
 
-Zombie::Zombie(std::string name_)
-{
-	name = name_;
-}
+Zombie::Zombie(std::string name_) { name = name_; }
 
-Zombie::~Zombie()
-{
-	std::cout << name << "is dead.." << std::endl;
-}
+Zombie::~Zombie() { std::cout << name << " is dead.." << std::endl; }
 
-void	Zombie::announce(std::string name)
-{
-	std::cout << name << ": BraiiiiiiinnnzzzZ" << std::endl;
-}
-
-Zombie* Zombie::newZombie( std::string name)
-{
-	Zombie *ret = new Zombie(name);
+void Zombie::announce() {
+  std::cout << this->name << ": BraiiiiiiinnnzzzZ" << std::endl;
 }
--- cpp01/ex00/Zombie.hpp
+++ cpp01/ex00/Zombie.hpp
@@ -1,13 +1,15 @@
 #include <string>
 
-class Zombie
-{
-private:
-	std::string name;
-public:
-	Zombie(/* args */);
-	Zombie(std::string name_);
-	~Zombie();
-	void	announce(std::string name);
-	Zombie* newZombie( std::string name);
+class Zombie {
+ private:
+  std::string name;
+
+ public:
+  Zombie(/* args */);
+  Zombie(std::string name_);
+  ~Zombie();
+  void announce();
 };
+
+Zombie* newZombie(std::string name);
+void randomChump(std::string name);
