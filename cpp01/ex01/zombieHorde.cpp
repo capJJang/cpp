a63de09a31ac2254ccda7720e7248ae0d79563f0
segan <>
2023-08-15 02:56:36 +0900

    fin : ex01

--- cpp01/ex01/zombieHorde.cpp
+++ cpp01/ex01/zombieHorde.cpp
@@ -1,18 +1,17 @@
-#include "Zombie.hpp"
 #include <iostream>
 
-Zombie* zombieHorde(int N, std::string name)
-{
-	Zombie *ret = new Zombie[N];
+#include "Zombie.hpp"
 
-	(void)name;
-	for (int i = 0; i < N; i++)
-	{
-		ret[i] = setZombieName(name + std::to_string(i + 1));
-		//ret[i] = Zombie(name + std::to_string(i + 1));
-		ret[i].announce();
-	}
-	return (ret);
+Zombie* zombieHorde(int N, std::string name) {
+  Zombie* ret = new Zombie[N];
+
+  (void)name;
+  for (int i = 0; i < N; i++) {
+    ret[i].setZombieName(name + std::to_string(i + 1));
+    // ret[i] = Zombie(name + std::to_string(i + 1));
+    ret[i].announce();
+  }
+  return (ret);
 }
 
-//scope를 벗어나면 소멸자 호출,,,
+// scope를 벗어나면 소멸자 호출,,,
