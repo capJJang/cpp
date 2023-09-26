f5431e3709a49507d18cd64a371529c96c93b009
segan <segan@42seoul.student.kr>
2023-09-26 18:56:57 +0900

    ...

---
+++ cpp07/ex00/whatever.h
@@ -0,0 +1,23 @@
+#ifndef WHATEVER_H
+#define WHATEVER_H
+
+template <typename T>
+void swap(T &x, T &y) {
+  T temp;
+
+  temp = x;
+  x = y;
+  y = temp;
+}
+
+template <typename T>
+T &min(T &x, T &y) {
+  return x <= y ? x : y;
+}
+
+template <typename T>
+T &max(T &x, T &y) {
+  return x > y ? x : y;
+}
+
+#endif
---
+++ cpp07/ex01/iter.h
@@ -0,0 +1,6 @@
+#ifndef ITER_H
+#define ITER_H
+
+template <typename T>
+
+#endif
